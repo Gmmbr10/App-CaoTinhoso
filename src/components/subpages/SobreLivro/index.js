0fce3b87daafe53fd3d853dccee892fd1e799bdc
gmmbr10 <giovannemmelo@gmail.com>
2023-09-03 12:11:31 -0300

    ajustando alguns textos

--- src/components/subpages/SobreLivro/index.js
+++ src/components/subpages/SobreLivro/index.js
@@ -15,25 +15,25 @@ export default function Book() {
                 <ScrollView>
                     <Text style={style.txt} >
 
-                        <Text style={style.subTitle}>Título</Text>: Nós Matamos o Cão-Tinhoso
+                        <Text style={style.subTitle}>Título</Text>: Nós Matamos o Cão-Tinhoso.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Autor</Text>: Luís Bernardo Honwana
+                        <Text style={style.subTitle}>Autor</Text>: Luís Bernardo Honwana.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Ano de publicação</Text>: 1964
+                        <Text style={style.subTitle}>Ano de publicação</Text>: 1964.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Gênero</Text>: Romance
+                        <Text style={style.subTitle}>Gênero</Text>: Romance.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Localização</Text>: Moçambique (período colonial)
+                        <Text style={style.subTitle}>Localização</Text>: Moçambique (período colonial).
                         {"\n\n"}
-                        <Text style={style.subTitle}>Temas abordados</Text>: Sociais, raciais, políticos, colonialismo, opressão, resistência
+                        <Text style={style.subTitle}>Temas abordados</Text>: Sociais, raciais, políticos, colonialismo, opressão, resistência.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Contexto histórico</Text>: Período colonial em Moçambique, então colônia portuguesa
+                        <Text style={style.subTitle}>Contexto histórico</Text>: Período colonial em Moçambique, então colônia portuguesa.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Relevância</Text>: Considerado um clássico da literatura africana e amplamente estudado e discutido
+                        <Text style={style.subTitle}>Relevância</Text>: Considerado um clássico da literatura africana e amplamente estudado e discutido.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Mensagem</Text>: Reflete sobre identidade, desigualdade e resistência na sociedade moçambicana
+                        <Text style={style.subTitle}>Mensagem</Text>: Reflete sobre identidade, desigualdade e resistência na sociedade moçambicana.
                         {"\n\n"}
-                        <Text style={style.subTitle}>Impacto</Text>: Reconhecido por sua mensagem política e socialmente engajada
+                        <Text style={style.subTitle}>Impacto</Text>: Reconhecido por sua mensagem política e socialmente engajada.
 
                     </Text>
                 </ScrollView>
